4eeadc5dade32de69005da85b94ece4c77ba60f0
oranguman158 <joseph@cantelmi.us>
2020-07-30 13:58:03 -0400

    Finished calculators with some flask but no back buttons

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
+from datetime import datetime
 from flask import Flask
 from flask import render_template
 from flask import request
 from model import get_amount_chicken
+from model import how_many_slaps
+
 
 
 # -- Initialization section --
@@ -14,11 +17,23 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", time=datetime.now())
 
 
 @app.route('/results', methods=['GET', 'POST'])
 def results():
     weight = request.form['weight']
     chickens = get_amount_chicken(weight)
-    return render_template("results.html", chickens=chickens, weight=weight)
+    return render_template("results.html", chickens=chickens, weight=weight, time=datetime.now())
+
+@app.route('/cooking', methods = ['GET', 'POST'])
+def cooking():
+    c_weight = request.form['c_weight']
+    if c_weight == "Rhode Island Weight":
+        c_weight = 3400
+    elif c_weight == "Plymouth Rock chicken":
+        c_weight = 3200
+    else:
+        c_weight = c_weight
+    slaps = how_many_slaps(float(c_weight))
+    return render_template("cook_results.html", slaps=slaps, c_weight=c_weight, time=datetime.now())
--- model.py
+++ model.py
@@ -1,2 +1,13 @@
 def get_amount_chicken(w):
-    return str(int(w)*5)
+    return int(w)*5
+
+def how_many_slaps(mass_chicken_g):
+    heat_capacity = 2.72
+    velocity = 4.0
+    mass_hand = 467.2
+    K = (1.0/2)*mass_hand*(velocity**2)
+    factor = 1.0/2
+    deltaT = (factor*K)/(mass_chicken_g*1000*heat_capacity)
+    #Assuming getting a chicken to 400 degrees cooks it
+    return 400.0/deltaT
+    #start = 23
